0c72016c3fca2c255b4fd8045dcde7e8bf4a664b
mdaucher2 <mdonner@htl-steyr.ac.at>
2018-12-07 13:36:51 +0100

    aaa

--- src/Reactiontest.java
+++ src/Reactiontest.java
@@ -20,7 +20,7 @@ public class Reactiontest extends JFrame {
         this.setLayout(new GridLayout(4,4));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // your mum gay
+        // bruder muss los
 
         // Buttons erzeugen
         for(int j = 0; j < button.length; j++){
